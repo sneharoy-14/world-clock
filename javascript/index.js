0a3e451957619decbb0745b888def236fd0a5f21
sneharoy-14 <sneharoy3104@gmail.com>
2023-05-31 13:58:09 +0100

    Added the functionality of the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,54 @@
 function updateTime() {
   // Kolkata
   let kolkataElement = document.querySelector("#kolkata");
-  let kolkataDateElement = kolkataElement.querySelector(".date");
-  let kolkataTimeElement = kolkataElement.querySelector(".time");
-  let kolkataTime = moment().tz("Asia/Kolkata");
+  if (kolkataElement) {
+    let kolkataDateElement = kolkataElement.querySelector(".date");
+    let kolkataTimeElement = kolkataElement.querySelector(".time");
+    let kolkataTime = moment().tz("Asia/Kolkata");
 
-  kolkataDateElement.innerHTML = moment().format("dddd D MMMM YYYY");
-  kolkataTimeElement.innerHTML = kolkataTime.format("HH:mm:ss");
-
+    kolkataDateElement.innerHTML = moment().format("dddd D MMMM YYYY");
+    kolkataTimeElement.innerHTML = kolkataTime.format("HH:mm:ss");
+  }
   // Istanbul
   let instanbulElement = document.querySelector("#istanbul");
-  let instanbulDateElement = instanbulElement.querySelector(".date");
-  let instanbulTimeElement = instanbulElement.querySelector(".time");
-  let instanbulTime = moment().tz("Asia/Istanbul");
+  if (instanbulElement) {
+    let instanbulDateElement = instanbulElement.querySelector(".date");
+    let instanbulTimeElement = instanbulElement.querySelector(".time");
+    let instanbulTime = moment().tz("Asia/Istanbul");
 
-  instanbulDateElement.innerHTML = moment().format("dddd D MMMM YYYY");
-  instanbulTimeElement.innerHTML = instanbulTime.format("HH:mm:ss");
-
+    instanbulDateElement.innerHTML = moment().format("dddd D MMMM YYYY");
+    instanbulTimeElement.innerHTML = instanbulTime.format("HH:mm:ss");
+  }
   // Prague
   let pragueElement = document.querySelector("#prague");
-  let pragueDateElement = pragueElement.querySelector(".date");
-  let pragueTimeElement = pragueElement.querySelector(".time");
-  let pragueTime = moment().tz("Europe/Prague");
+  if (pragueElement) {
+    let pragueDateElement = pragueElement.querySelector(".date");
+    let pragueTimeElement = pragueElement.querySelector(".time");
+    let pragueTime = moment().tz("Europe/Prague");
 
-  pragueDateElement.innerHTML = moment().format("dddd D MMMM YYYY");
-  pragueTimeElement.innerHTML = pragueTime.format("HH:mm:ss");
+    pragueDateElement.innerHTML = moment().format("dddd D MMMM YYYY");
+    pragueTimeElement.innerHTML = pragueTime.format("HH:mm:ss");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("dddd D MMMM YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("HH:mm:ss")}</div>
+</div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
